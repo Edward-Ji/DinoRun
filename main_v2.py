da4829d909974a89ad0ef1ee943c5e112cd4ab7d
Edward Ji <jiziao6@gmail.com>
2018-11-22 17:52:41 +1100

    Renew the thingy

--- main_v2.py
+++ main_v2.py
@@ -1,62 +1,152 @@
 import pygame
 from pygame.locals import *
-import random
+import os
+import sys
 
+
+# global constants
 WIDTH = 960
 HEIGHT = 240
-GROUND = 30
 FPS = 30
+RES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "res")
+
+def res(fname):
+    return os.path.join(RES, fname)
+
 
 # define colors
+ALPHA = (255, 255, 255, 255)
 WHITE = (255, 255, 255)
+GREY = (155, 155, 155)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+DIRT = (153, 102, 51)
+
+# game constants
+RUN = 0
+DOWN = 1
+JUMP = 2
+
+GROUNDH = 30
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(WHITE)
 pygame.display.set_caption("My Game")
+
 clock = pygame.time.Clock()
 
+stick_img = {RUN: (pygame.image.load(res("stick_run_1.png")),
+                   pygame.image.load(res("stick_run_2.png"))),
+             DOWN: (pygame.image.load(res("stick_down_1.png")),
+                    pygame.image.load(res("stick_down_2.png"))),
+             JUMP: pygame.image.load(res("stick_jump.png"))}
 
-stick_run_img = (pygame.image.load(res("stick_run_1.png")),
-                 pygame.image.load(res("stick_run_2.png")))
-stick_down_img = (pygame.image.load(res("stick_down_1.png")),
-                 pygame.image.load(res("stick_down_2.png")))
-stick_jump_img = pygame.image.load(res("stick_jump.png"))
+cactus_img = pygame.image.load(res("cactus.png"))
 
 
-ground = pygame.Surface(0, )
+class GameObj(pygame.sprite.Sprite):
+
+    all = pygame.sprite.RenderUpdate()
+
+    def __init__(self):
+        super().__init__()
+        GameObj.all.add(self)
+
+
+class Ground(GameObj):
+
+    all = pygame.sprite.Group()
+
+    def __init__(self, x_disp=0):
+        super().__init__()
+        self.image = pygame.Surface((WIDTH, GROUNDH))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = HEIGHT
+        self.rect.move_ip(x_disp, 0)
+        self.rand()
+
+    def rand(self):
+        self.image.fill(DIRT)
+        for dot_y in range(0, 10):
+            color = mix_color(DIRT, GREY, dot_y / 10)
+            pygame.draw.line(self.image, color, (0, dot_y), (WIDTH, dot_y), 1)
+
+    def update(self):
+        self.rect.move_ip(-Player.SPEED, 0)
+        if self.rect.right == 0:
+            self.rect.left = WIDTH
+            self.rand()
+
+
+class Block:
+
+    all = pygame.sprite.Group()
+
+    def __init__(self):
+        super().__init__()
+        self.image = cactus_img
+        self.rect = self.image.get_rect()
+        self.rect.bottom = GROUNDH
+        Block.all.add(self)
 
 
 class Player(pygame.sprite.Sprite):
+    SPEED = 10
     X_OFFSET = 100
 
+    r = None
+
     def __init__(self):
-        self.image = stick_run_img[0]
+        super().__init__()
+        self.image = stick_img[RUN][0]
         self.rect = self.image.get_rect()
-        self.rect.bottom = GROUND
+        self.rect.bottom = GROUNDH
         self.rect.centerx = Player.X_OFFSET
+        Player.r = self
+
+    def update(self):
+        pass
 
 
-all_sprites = pygame.sprite.Group()
+def mix_color(color_a, color_b, ratio=0.5):
+    return [int(a * ratio + b * (1 - ratio)) for a, b in zip(color_a, color_b)]
 
-running = True
-while running:
 
-    clock.tick(FPS)
+def main():
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    Ground()
+    Ground(WIDTH)
 
-    all_sprites.update()
+    running = True
+    while running:
 
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    pygame.display.flip()
+        clock.tick(FPS)
 
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_all()
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_q and event.mod & KMOD_META:
+                    quit_all()
+                if event.key == K_SPACE:
+                    input()
+
+        GameObj.all.update()
+
+        screen.fill(WHITE)
+        GameObj.all.draw(screen)
+        pygame.display.flip()
+
+
+def quit_all():
+    pygame.quit()
+    sys.exit()
+
+
+main()
