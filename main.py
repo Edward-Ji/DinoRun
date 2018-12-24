ebb5cf179ae78a0532ed90b7a62820b3f5eefa86
Edward Ji <jiziao6@gmail.com>
2018-12-24 21:12:39 +0800

    shop interface completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import pygame
+from pygame.locals import *
+
 import os
 import random
 import sys
 
-import pygame
-from pygame.locals import *
 
 # pygame constants
 VER = "0.2"
@@ -12,11 +13,17 @@ WIDTH = 960
 HEIGHT = 240
 FPS = 60
 ANIMATE = 5
+BUTTONCLICK = USEREVENT + 1
 RES = os.path.join(os.path.dirname(os.path.realpath(__file__)), "res")
+OBS = os.path.join(RES, "obs")
 
 
-def res(file_name):
-    return os.path.join(RES, file_name)
+def res(file):
+    return os.path.join(RES, file)
+
+
+def obs(file):
+    return os.path.join(OBS, file)
 
 
 # define colors
@@ -49,15 +56,17 @@ BLOCK_GAP_LIMIT = 240
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(WHITE)
 pygame.display.set_caption("Stick Run" + " V" + VER)
+icon_img = pygame.image.load(res("icon.png"))
+pygame.display.set_icon(icon_img)
 
 clock = pygame.time.Clock()
 
 # font
-SIGN_FONT = pygame.font.Font(None, 20)
+SIGN_FONT = pygame.font.Font(None, 21)
 TITLE_FONT = pygame.font.Font(None, 65)
 NORMAL_FONT = pygame.font.Font(None, 30)
+BUTTON_FONT = pygame.font.Font(None, 45)
 
 # load images
 sign_img = pygame.image.load(res("sign.png"))
@@ -66,8 +75,97 @@ stick_img = {RUN: (pygame.image.load(res("stick_run_1.png")),
              DOWN: (pygame.image.load(res("stick_down_1.png")),
                     pygame.image.load(res("stick_down_2.png"))),
              JUMP: pygame.image.load(res("stick_jump.png"))}
-cactus_img = pygame.image.load(res("cactus.png"))
 timer_img = pygame.image.load(res("timer.png"))
+shop_btn_img = pygame.image.load(res("shop_btn.png"))
+set_btn_img = pygame.image.load(res("set_btn.png"))
+tick_img = pygame.image.load(res("tick.png"))
+cross_img = pygame.image.load(res("cross.png"))
+
+block_img = {}
+for file_name in os.listdir(OBS):
+    name_text = file_name.split('.')[0]
+    block_img[file_name] = pygame.image.load(obs(file_name))
+
+# load settings file
+
+
+class Button(pygame.sprite.DirtySprite):
+
+    all = pygame.sprite.Group()
+
+    def __init__(self, text, img, rect):
+        super().__init__()
+        self._img = img
+        self.text = text
+        self.image = img
+        self.rect = pygame.Rect(rect)
+        Button.all.add(self)
+
+    def update(self):
+        if not self.rect.collidepoint(*mouse_pos):
+            self.image = pygame.Surface(self._img.get_size(), SRCALPHA)
+            self.image.blit(self._img, (0, 0))
+        else:
+            size = list(self._img.get_size())
+            size = [int(1.2 * val) for val in size]
+            self.image = pygame.transform.scale(self._img, size)
+            if mouse_press[0]:
+                event = pygame.event.Event(BUTTONCLICK, {"text": self.text})
+                pygame.event.post(event)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+
+class Check(pygame.sprite.Sprite):
+
+    states = {}
+
+    GAP = 10
+    left = GAP
+
+    all = pygame.sprite.Group()
+
+    def __init__(self, img_name, state):
+        super().__init__()
+        self._state = state
+        self._img = block_img[img_name]
+        self.image = None
+        self.state = state
+        self.rect.left = Check.left
+        Check.left += self.rect.width + Check.GAP
+        Check.all.add(self)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        if value:
+            state_img = tick_img
+        else:
+            state_img = cross_img
+        state_rect = state_img.get_rect()
+        surf_width, surf_height = self._img.get_size()
+        surf_height += state_rect.height
+        state_rect.centerx = int(surf_width / 2)
+        state_rect.bottom = surf_height
+        self.image = pygame.Surface((surf_width, surf_height), SRCALPHA)
+        self.image.blit(self._img, (0, 0))
+        self.image.blit(state_img, state_rect)
+        if hasattr(self, "rect"):
+            center = self.rect.center
+        else:
+            center = (30, 30)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self._state = value
+
+    def update(self):
+        if self.rect.collidepoint(mouse_pos):
+            if mouse_press[0] and mouse_down:
+                self.state = not self.state
 
 
 class GameObj(pygame.sprite.Sprite):
@@ -77,17 +175,6 @@ class GameObj(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         GameObj.all.add(self)
-
-
-class Button(GameObj):
-
-    all = pygame.sprite.Group()
-
-    def __init__(self):
-        pass
-
-    def update(self, *args):
-        pass
 
 
 class Timer(GameObj):
@@ -121,11 +208,13 @@ class Timer(GameObj):
 
 class Sign(GameObj):
 
+    last = 0
+
     all = pygame.sprite.Group()
 
     def __init__(self, text):
         super().__init__()
-        self.image = sign_img
+        self.image = sign_img.copy()
         bit_map = SIGN_FONT.render(text, True, WHITE)
         text_rect = bit_map.get_rect()
         text_rect.center = 25, 12
@@ -141,6 +230,13 @@ class Sign(GameObj):
         self.rect.move_ip(-Player.SPEED, 0)
         if self.rect.right < 0:
             self.kill()
+
+    @classmethod
+    def gene(cls):
+        distance = round(Player.r.distance / 10000, 1)
+        if distance % 1.0 == 0.0 and distance != Sign.last:
+            cls(str(round(distance)) + "km")
+            cls.last = distance
 
 
 class Ground(GameObj):
@@ -176,12 +272,14 @@ class Ground(GameObj):
 
 
 class Block(GameObj):
+
     last = 0
 
     all = pygame.sprite.Group()
 
-    def __init__(self, img):
+    def __init__(self):
         super().__init__()
+        img = Block.random()
         pile = choose({1: 0.5, 2: 0.35, 3: 0.15})
         single_width = img.get_width()
         single_height = img.get_height()
@@ -200,13 +298,17 @@ class Block(GameObj):
             self.kill()
 
     @classmethod
+    def random(cls):
+        return block_img[random.choice(list(block_img.keys()))]
+
+    @classmethod
     def gene(cls):
         if cls.last > BLOCK_GAP_LIMIT:
             if len(cls.all.sprites()) == 0:
-                cls(cactus_img)
+                cls()
             elif len(cls.all.sprites()) < 3:
                 if random.random() < 0.01:
-                    cls(cactus_img)
+                    cls()
         cls.last += Player.SPEED
 
 
@@ -238,9 +340,9 @@ class Player(GameObj):
             if key_state[K_UP] or key_state[K_w]:
                 self._state = JUMP
                 self._speed_y = Player.JUMP_POWER
-            elif key_state[K_DOWN] or key_state[K_s]:
-                self._state = DOWN
-                Player.SPEED = DOWN_SPEED
+            # elif key_state[K_DOWN] or key_state[K_s]:
+            #     self._state = DOWN
+            #     Player.SPEED = DOWN_SPEED
             else:
                 self._state = RUN
                 Player.SPEED = RUN_SPEED
@@ -263,10 +365,12 @@ class Player(GameObj):
         self.distance += Player.SPEED
 
 
+# mix two given color by a certain ratio
 def mix_color(color_a, color_b, ratio=0.5):
     return [int(a * ratio + b * (1 - ratio)) for a, b in zip(color_a, color_b)]
 
 
+# choose from the keys by value (chance) in a dictionary
 def choose(dct):
     rand = random.random()
     bar = 0
@@ -277,6 +381,7 @@ def choose(dct):
     raise ValueError('total possibility unreachable')
 
 
+# show a pop-up dialogue and request keyboard input
 def dialogue(display,
              title="Dialogue",
              msg="Please select one from the following options",
@@ -331,51 +436,118 @@ def dialogue(display,
         clock.tick(FPS)
 
         for event in pygame.event.get():
-            if event.type == QUIT:
-                quit_all()
-            elif event.type == KEYDOWN:
-                if event.key == K_q and event.mod & KMOD_META:
-                    quit_all()
+            if event.type == KEYDOWN and not event.mod:
                 if event.key in key_binding.keys():
                     return key_binding.get(event.key)
 
 
 def main():
 
+    global key_state, mouse_press, mouse_pos
+
     # preparing images
     cactus_loc = []
     for i in range(35):
+        img = Block.random()
         x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-        cactus_loc.append((x, y))
-    bit_map = TITLE_FONT.render("SPACE to START!", True, BLACK)
+        cactus_loc.append((img, (x, y)))
+    bit_map = TITLE_FONT.render("<- SPACE to START! ->", True, BLACK)
     text_rect = bit_map.get_rect()
     text_rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    shop_btn_rect = shop_btn_img.get_rect()
+    shop_btn_rect.left = 10
+    shop_btn_rect.centery = HEIGHT / 2
+    Button("shop", shop_btn_img, shop_btn_rect)
+
+    set_btn_rect = set_btn_img.get_rect()
+    set_btn_rect.right = WIDTH - 10
+    set_btn_rect.centery = HEIGHT / 2
+    Button("settings", set_btn_img, set_btn_rect)
 
     running = True
     while running:
 
         clock.tick(FPS)
 
-        screen.fill(SKY)
-        for loc in cactus_loc:
-            screen.blit(cactus_img, loc)
-        screen.blit(bit_map, text_rect)
-        pygame.display.flip()
-
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 quit_all()
-            if event.type == pygame.KEYDOWN:
+            elif event.type == KEYDOWN:
                 if event.key == K_q and event.mod & KMOD_META:
                     quit_all()
-                if event.key == K_SPACE:
+                elif event.key == K_SPACE:
                     answer = "Retry"
                     while answer == "Retry":
                         score = start()
                         answer = dialogue(screen,
                                           "Play again?",
-                                          "You got " + str(score) + " points in your last run!",
+                                          "You reached " + str(score) + " km in your last run!",
                                           {K_SPACE: "Retry", K_q: "Main Menu"})
+                elif event.key == K_LEFT:
+                    shop()
+                elif event.key == K_RIGHT:
+                    settings()
+            elif event.type == BUTTONCLICK:
+                if event.text == "shop":
+                    shop()
+                if event.text == "settings":
+                    settings()
+
+        key_state = pygame.key.get_pressed()
+        mouse_press = pygame.mouse.get_pressed()
+        mouse_pos = pygame.mouse.get_pos()
+
+        screen.fill(SKY)
+
+        for img, loc in cactus_loc:
+            screen.blit(img, loc)
+        screen.blit(bit_map, text_rect)
+        Button.all.update()
+        Button.all.draw(screen)
+
+        pygame.display.flip()
+
+
+def shop():
+
+    global key_state, mouse_press, mouse_pos, mouse_down
+
+    for img_name, state in block_img.items():
+        Check(img_name, state)
+
+    running = True
+    while running:
+
+        mouse_down = False
+
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit_all()
+            elif event.type == KEYDOWN:
+                if event.key == K_q and event.mod & KMOD_META:
+                    quit_all()
+                elif event.key == K_SPACE:
+                    return
+            elif event.type == MOUSEBUTTONDOWN:
+                mouse_down = True
+
+        key_state = pygame.key.get_pressed()
+        mouse_press = pygame.mouse.get_pressed()
+        mouse_pos = pygame.mouse.get_pos()
+
+        screen.fill(SKY)
+
+        Check.all.update()
+        Check.all.draw(screen)
+
+        pygame.display.flip()
+
+
+def settings():
+    print("settings!")
 
 
 def start():
@@ -429,16 +601,18 @@ def start():
             screen.blit(pause_text, pause_rect)
         elif Timer.end:
             Block.gene()
+            Sign.gene()
             GameObj.all.update()
         else:
             timer.update()
 
         if pygame.sprite.spritecollide(Player.r, Block.all, False):
-            score = Player.r.distance
+            score = round(Player.r.distance / 10000, 1)
             Player.r.kill()
             Player.r = None
             for block in Block.all.sprites():
                 block.kill()
+            Sign.last = 0
             if not Timer.end:
                 timer.kill()
             return score
@@ -447,15 +621,15 @@ def start():
         pygame.display.update(dirty)
 
 
-def quit_all():
-    answer = dialogue(screen,
-                      title="Warning",
-                      msg="Do you really want to quit?")
-    if answer == "Yes":
+def quit_all(condition=False):
+    if condition:
         pygame.quit()
         sys.exit()
-    else:
-        Player.pause = True
+    elif dialogue(screen,
+                  title="Warning",
+                  msg="Do you really want to quit?") == "Yes":
+        pygame.quit()
+        sys.exit()
 
 
 main()
